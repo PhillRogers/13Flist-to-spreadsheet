1aa374a61288e884dfb5176498a9223380dff7ab
PhillRogers <85609783+PhillRogers@users.noreply.github.com>
2025-10-03 13:55:36 +0100

    added license

--- SEC-13Flist-ToSpreadsheet.py
+++ SEC-13Flist-ToSpreadsheet.py
@@ -12,7 +12,24 @@
 #	2025-10-03    Phill
 #   Assume downloaded PDF is in same folder as this script. If not then launch browser to get one.
 #   Add XLSX file output.  Tested with auto-py-to-exe  Check with pylint = 10.0
+# License:
+#   SEC-13Flist-ToSpreadsheet - Parse SEC 13flist PDF to spreadsheet format.
+#   Copyright (C) 2025  Phill Rogers
 #
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#   PRogers at Enhance.Group
 
 import csv
 import glob
